59d806579db9b0c3dd9f3da48b90e9a7b41f3d57
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-06-28 16:07:56 +0200

    install the jwt auth and implement code for generationg the jwt token and set cookie

---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    expiresIn: '24h',
+  })
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 24 * 60 * 60 * 1000
+  })
+
+  return token
+}
+
+export default generateTokenAndSetCookie
