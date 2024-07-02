cd6d03d3f97fd8268440e46f6410af284ac4eef8
RAMCHARAN HANUMANTHU <106550364+HRC-123@users.noreply.github.com>
2024-07-02 10:52:40 +0530

    Added ToolBar,Run,TestCase (#33)

--- frontend/src/Components/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import DropDown from "./DropDown";
 import { CODE_SNIPPETS} from "../Utils/languages"
-
+import Run from "./Run";
 
 const CodeEditor = () => {
   const editorRef = useRef();
@@ -20,25 +20,26 @@ const CodeEditor = () => {
   };
 
   return (
-   <div> 
-        
-          <DropDown language={language} onSelect={onSelect} />
-          <Editor
-            options={{
-              minimap: {
-                enabled: false,
-              },
-            }}
-            height="75vh"
-            theme="vs-dark"
-            language={language}
-            defaultValue={CODE_SNIPPETS[language]}
-            onMount={onMount}
-            value={value}
-            onChange={(value) => setValue(value)}
-          />
-       </div>
-   
+    <div>
+      <div className="flex justify-between mr-4 items-center">
+        <DropDown language={language} onSelect={onSelect} />
+        <Run />
+      </div>
+      <Editor
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+        height="75vh"
+        theme="vs-dark"
+        language={language}
+        defaultValue={CODE_SNIPPETS[language]}
+        onMount={onMount}
+        value={value}
+        onChange={(value) => setValue(value)}
+      />
+    </div>
   );
 };
 export default CodeEditor;
