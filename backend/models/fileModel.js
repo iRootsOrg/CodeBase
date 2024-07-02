f69c1048da7822e23204ef956e83710dc5747c88
Himanshu <96365482+17himanshu@users.noreply.github.com>
2024-07-02 10:14:20 +0530

    changed fileSchema

--- backend/models/fileModel.js
+++ backend/models/fileModel.js
@@ -1,5 +1,30 @@
 const mongoose = require("mongoose");
 
+const inputOutputSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['file', 'direct'],
+        required: true
+    },
+    reference: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File',
+        required: function() { return this.type === 'file'; }
+    },
+    content: {
+        type: String,
+        required: function() { return this.type === 'direct'; } 
+    },
+    _id:false
+});
+
+const testCaseSchema = new mongoose.Schema({
+    inputs: [inputOutputSchema],
+    outputs: [inputOutputSchema],
+    _id: false
+});
+
+
 const fileSchema = new mongoose.Schema({
     filename:{
         type:String,
@@ -13,6 +38,12 @@ const fileSchema = new mongoose.Schema({
         type: Buffer,
         required:true
     }, 
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        unique:true,
+        required: true,
+    },
     createdAt: {
         type:Date,
         required:true
@@ -21,17 +52,14 @@ const fileSchema = new mongoose.Schema({
         type:Date,
         default: Date.now
     },
-    author: {
-        type:String,
-        required: true
-    },
     tags: {
         type:[String],
         unique: true
     }, 
     description: {
         type:String
-    } 
+    },
+    testCases: [testCaseSchema]
 });
 
 const File = mongoose.model("File", fileSchema);
