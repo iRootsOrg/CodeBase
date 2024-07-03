c3aa0e043f521be770ec820680dfc0e36bd7214d
Kunal Singla <100127366+KS963000@users.noreply.github.com>
2024-07-04 00:00:43 +0530

    Authentication Routes (#45)
    * added User Routes and JWT
    
    * userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { registerUser, loginUser } = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/signup", registerUser);
-router.post("/signin", loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router
