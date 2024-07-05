89fde9a10be36590b38173755cb7fce30e9ae4eb
KS963000 <kunalsingla.9630@gmail.com>
2024-07-05 13:40:24 +0530

    added deleteRole and modifyRole

--- backend/controllers/roleController.js
+++ backend/controllers/roleController.js
@@ -2,36 +2,33 @@ const Role = require('../models/roleModel');
 
 class RoleController {
     async createRoles() {
-        const authorRole = new Role({
-            name: 'author',
-            permissions: ['CREATE', 'READ', 'UPDATE', 'DELETE']
-        });
+        const rolesToCreate = [
+            { name: 'author', permissions: ['CREATE', 'READ', 'UPDATE', 'DELETE'] },
+            { name: 'reviewer', permissions: ['READ', 'RUN_TESTCASES'] },
+            { name: 'editor', permissions: ['READ', 'UPDATE'] },
+            { name: 'co-author', permissions: ['READ', 'CONTRIBUTE'] },
+            { name: 'admin', permissions: ['CREATE', 'READ', 'UPDATE', 'DELETE', 'RUN_TESTCASES', 'CONTRIBUTE', 'MANAGE_PERMISSIONS'] },
+        ];
 
-        const reviewerRole = new Role({
-            name: 'reviewer',
-            permissions: ['READ', 'RUN_TESTCASES']
-        });
+        for (const roleData of rolesToCreate) {
+            const existingRole = await Role.findOne({ name: roleData.name });
+            if (!existingRole) {
+                const newRole = new Role(roleData);
+                await newRole.save();
+            }
+        }
+    }
 
-        const editorRole = new Role({
-            name: 'editor',
-            permissions: ['READ', 'UPDATE']
-        });
+    async deleteRole(roleName) {
+        await Role.findOneAndRemove({ name: roleName });
+    }
 
-        const coAuthorRole = new Role({
-            name: 'co-author',
-            permissions: ['READ','CONTRIBUTE']
-        });
-
-        const adminRole = new Role({
-            name: 'author',
-            permissions: ['CREATE', 'READ', 'UPDATE', 'DELETE', 'RUN_TESTCASES', 'CONTRIBUTE', 'MANAGE_PERMISSIONS']
-        });
-
-        await authorRole.save();
-        await reviewerRole.save();
-        await editorRole.save();
-        await coAuthorRole.save();
-        await adminRole.save();
+    async modifyRole(roleName, updatedPermissions) {
+        const role = await Role.findOne({ name: roleName });
+        if (role) {
+            role.permissions = updatedPermissions;
+            await role.save();
+        }
     }
 }
 
