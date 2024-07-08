17823093315bc8d57c462e8e125512774d4bacaa
KS963000 <kunalsingla.9630@gmail.com>
2024-07-08 14:38:34 +0530

    added /health-check

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,11 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path")
-const fileRoutes=require("./routes/fileRoutes")
+const fileRoutes = require("./routes/fileRoutes")
 const userRoutes = require("./routes/userRoutes")
-const connectDB=require("./config/db")
+const connectDB = require("./config/db")
 const fileUpload = require("express-fileupload");
-
+const WebSocket = require("ws");
 
 const app = express();
 dotenv.config();
@@ -20,8 +20,37 @@ app.use(express.json());
 app.use(fileUpload());
 
 const PORT = process.env.PORT;
+const wssPort = process.env.WSS_PORT;
+// console.log(wssPort)
+const wss = new WebSocket.Server({ port: wssPort });
+// console.log(wss)
+wss.on("connection", (ws) => {
+    console.log("Client connected to WebSocket server");
 
-app.use("/api/v1/file",fileRoutes);
+    ws.on("message", (message) => {
+        console.log(`Received message => ${message}`);
+    });
+
+    ws.on("error", (error) => {
+        console.log("Error occurred in WebSocket connection");
+        console.error(error);
+    });
+
+    ws.on("close", () => {
+        console.log("Client disconnected from WebSocket server");
+    });
+});
+
+app.get('/health-check', (req, res) => {
+    if (wss && wss.listeners('connection').length > 0) {
+        res.status(200).send('WebSocket server is running');
+    } else {
+        res.status(500).send('WebSocket server is not running');
+    }
+});
+
+
+app.use("/api/v1/file", fileRoutes);
 app.use("/api/v1/user", userRoutes);
 
 app.get("/", (req, res) => {
