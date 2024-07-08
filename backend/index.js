47349e1f95765941a8f7afdf59f3a3843463d6e7
Himanshu <96365482+17himanshu@users.noreply.github.com>
2024-07-08 20:29:23 +0530

    Resolved issue #53 (#62)
    * backend changes
    
    * made centralise error handling using winston

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const fileRoutes=require("./routes/fileRoutes")
 const userRoutes = require("./routes/userRoutes")
 const connectDB=require("./config/db")
 const fileUpload = require("express-fileupload");
-
+const errorMiddleware = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
@@ -19,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
+
 const PORT = process.env.PORT;
 
 app.use("/api/v1/file",fileRoutes);
@@ -29,6 +30,9 @@ app.get("/", (req, res) => {
         message: "Welcome to compiler backend",
     });
 });
+
+
+app.use(errorMiddleware); 
 
 // listen server
 app.listen(PORT, () => {
