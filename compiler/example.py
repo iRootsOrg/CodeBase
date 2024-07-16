df23ea6e3dea3252c5514a67915752becebf2366
Raghav Sapra <raghavsapraofficial@gmail.com>
2024-07-16 13:41:41 +0530

    Memory Limit Changes

--- compiler/example.py
+++ compiler/example.py
@@ -1,4 +1,9 @@
-i =1
-while i < 10:
-    print("number: " , i)
-    i+=1
+a = []
+try:
+    i=1
+    while i<10:
+        i+=1
+        a.append(' ' * 10**1)
+        print("Memory used: {}".format(len(a)))
+except MemoryError:
+    print("Memory used: {}".format(len(a) * 10**6))
