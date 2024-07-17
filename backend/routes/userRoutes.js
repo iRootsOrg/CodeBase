c867d0ba5d2dea52aad85ba2bb8cfc6d488cb356
Himanshu <96365482+17himanshu@users.noreply.github.com>
2024-07-18 00:50:48 +0530

    Merged changes from backend-4 branch to backend folder

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const authenticateToken = require("../middlewares/authMiddleware");
 const { registerUser,
     loginUser,
     assignRole,
+    startEditingSession, 
+    joinEditingSession,
     getUserRoles,
     removeRoleFromUser,
     addCoauthor
@@ -12,10 +15,11 @@ const router = express.Router();
 
 router.post("/signup", registerUser);
 router.post("/signin", loginUser);
+router.post('/start-editing-session', authenticateToken, startEditingSession)
+router.post('/join-editing-session', authenticateToken, joinEditingSession)
 router.post("/assign-role", assignRole);
 router.get("/user-roles/:userId", getUserRoles);
 router.post("/delete-role", removeRoleFromUser);
-router.post("/add-coauthor",authMiddleware,addCoauthor);
-
+router.post("/add-coauthor",authenticateToken,addCoauthor);
 
 module.exports = router
