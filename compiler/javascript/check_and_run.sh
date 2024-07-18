822969bde93adede5f5beb1ff8a39855b58f559e
dushyant-sharma937 <dushyants.ec.21@nitj.ac.in>
2024-07-19 01:23:25 +0530

    upto continuously file checking automation, not optimization

--- compiler/javascript/check_and_run.sh
+++ compiler/javascript/check_and_run.sh
@@ -17,7 +17,7 @@ for code_file in "$INPUT_DIR"/codefile*.js; do
     # Run the code file if output doesn't exist or input file is newer
     if [ ! -f "$output_file" ] || [ "$input_file" -nt "$output_file" ]; then
         echo "Running code file: $code_file with input file: $input_file"
-        /usr/src/app/runner.sh
+        /usr/src/app/runner.sh "$code_file" "$input_file" "$output_file"
     else
         echo "No changes detected for: $code_file"
     fi
