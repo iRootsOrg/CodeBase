b2edd6af977e788a37162dbcb20414cd6e1d9e4c
KS963000 <kunalsingla.9630@gmail.com>
2024-07-18 16:28:17 +0530

    Merge upstream backend changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,25 +1,26 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
+const authenticateToken = require("../middlewares/authMiddleware");
 const { registerUser,
     loginUser,
     assignRole,
+    startEditingSession, 
+    joinEditingSession,
     getUserRoles,
     removeRoleFromUser,
     addCoauthor
 } = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
-const RoleAssignmentController = require("../controllers/roleAssignmentController");
-const roleAssignmentController = new RoleAssignmentController();
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
 router.post("/signin", loginUser);
+router.post('/start-editing-session', authenticateToken, startEditingSession)
+router.post('/join-editing-session', authenticateToken, joinEditingSession)
 router.post("/assign-role", assignRole);
 router.get("/user-roles/:userId", getUserRoles);
 router.post("/delete-role", removeRoleFromUser);
-router.post("/add-coauthor",authMiddleware,addCoauthor);
-router.post('/assign-role', roleAssignmentController.assignRoleToUser);
-router.post('/remove-role', roleAssignmentController.removeRoleFromUser);
-router.get('/user-roles/:userId', roleAssignmentController.getUserRoles);
+router.post("/add-coauthor",authenticateToken,addCoauthor);
 
 module.exports = router
