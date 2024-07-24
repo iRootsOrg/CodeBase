dad82ea3f3834df2ce4fa2ef5d66209fcfa34ca3
KS963000 <kunalsingla.9630@gmail.com>
2024-07-24 21:12:20 +0530

    implement search algorithm

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authenticateToken = require("../middlewares/authMiddleware");
 const {
     uploadController,
-    decodeController
+    decodeController,
+    searchFiles
 } = require("../controllers/fileController");
 const router = express.Router();
 
 router.post("/upload",authenticateToken,uploadController);
 router.get("/decode/:id",decodeController);
+router.get("/search", searchFiles);
 
 module.exports = router;
