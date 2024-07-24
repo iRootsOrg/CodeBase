ae14d6f2c0f9aa6f57660b71205a901d23f25196
Himanshu <96365482+17himanshu@users.noreply.github.com>
2024-07-24 11:21:21 +0530

    added project model and modified fileController

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const authenticateToken = require("../middlewares/authMiddleware");
 const {
     uploadController,
-    decodeController
+    decodeController,
+    getFolderStructureController
 } = require("../controllers/fileController");
 const router = express.Router();
 
 router.post("/upload",authenticateToken,uploadController);
 router.get("/decode/:id",decodeController);
+router.get("/file-structure/:projectId",authenticateToken, getFolderStructureController);
 
 module.exports = router;
