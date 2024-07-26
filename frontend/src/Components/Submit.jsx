f32d6c26963cc37429b4e855cdaeb97819f5668c
pratham9770 <118453138+pratham9770@users.noreply.github.com>
2024-07-26 23:01:20 +0530

    Issues Resolved (#159)
    * frontend for code reviewer
    
    * Code Visibility blurred and Output window added
    
    * merging conflicts
    
    * added functionalities
    
    * regarding issues
    
    * merged conflicts
    
    * merged conflicts
    
    * chnages in code editor

--- frontend/src/Components/Submit.jsx
+++ frontend/src/Components/Submit.jsx
@@ -1,12 +1,10 @@
-
-
-
-const Submit = (props) => {
-  const onSubmit = () => {
-    console.log("Submitting");
-  };
+const Submit = ({ onSubmit,lightmode }) => {
+  
+  
+  
   return (
     <button
+
       className={`block h-full w-24 px-2 py-1 text-center rounded ${props.lightmode ? "bg-custom-gradient":"bg-custom-gradient-inverted"} text-white`}
       onClick={() => {
         onSubmit();
