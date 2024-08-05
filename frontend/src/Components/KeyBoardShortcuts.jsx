2963923f0463dc7cf758282efa2678574be81cfc
Ramcharan <ramcharan2k4@gmail.com>
2024-08-05 10:54:42 +0530

    Changed commands according to os

--- frontend/src/Components/KeyBoardShortcuts.jsx
+++ frontend/src/Components/KeyBoardShortcuts.jsx
@@ -1,8 +1,19 @@
 
 import {  FaTimes } from "react-icons/fa";
-
+import { useState,useEffect } from "react";
 const KeyBoardShortcuts = (props) => {
     
+  const [os, setOs] = useState("");
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+     if (userAgent.indexOf("Win") !== -1) setOs("Windows");
+     if (userAgent.indexOf("Mac") !== -1) setOs("MacOS");
+     if (userAgent.indexOf("X11") !== -1) setOs("UNIX");
+     if (userAgent.indexOf("Linux") !== -1) setOs("Linux");
+
+     
+  }, []);
 
     return (
       <div className="fixed bottom-0 w-full h-full sm:w-auto sm:h-auto sm:inset-0 flex items-end sm:items-center sm:justify-center ">
@@ -53,171 +64,171 @@ const KeyBoardShortcuts = (props) => {
                 <tr>
                   <td className="border px-4 py-2">Command Palette</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + Shift + P</p>
-                    <p>Cmd + Shift + P</p>
+                    {os ? (<p>Ctrl + Shift + P</p>) :
+                      (<p>Cmd + Shift + P</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To access all available commands and features in VS Code.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Quick Open</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + P</p>
-                    <p>Cmd + P</p>
+                    {os ? (<p>Ctrl + P</p>) :
+                      (<p>Cmd + P</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To quickly open files by typing their name.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Find</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + F</p>
-                    <p>Cmd + F</p>
+                    {os ? (<p>Ctrl + F</p>)
+                      : (<p>Cmd + F</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To find a string within the current file.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
-                <tr>
+                {/* <tr>
                   <td className="border px-4 py-2">Toggle Terminal</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + `</p>
+                    {os ? (<p>Ctrl + `</p>):{}
                   </td>
                   <td className="border px-4 py-2">
                     To show or hide the integrated terminal.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
-                </tr>
+                  <td className="border px-4 py-2">{os}</td>
+                </tr> */}
                 <tr>
                   <td className="border px-4 py-2">Save</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + S</p>
-                    <p>Cmd + S</p>
+                    {os ? (<p>Ctrl + S</p>)
+                      : (<p>Cmd + S</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To save the current file.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Open Settings</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + ,</p>
-                    <p>Cmd + ,</p>
+                    {os ? (<p>Ctrl + ,</p>):
+                    (<p>Cmd + ,</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To open the user settings configuration.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Format Document</td>
                   <td className="border px-4 py-2">
-                    <p>Shift + Alt + F</p>
-                    <p>Shift + Option + F</p>
+                    {os ? (<p>Shift + Alt + F</p>) :
+                      (<p>Shift + Option + F</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To automatically format the entire document according to the
                     language's formatting rules.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Comment Line</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + /</p>
-                    <p>Cmd + /</p>
+                    {os ? (<p>Ctrl + /</p>) :
+                      (<p>Cmd + /</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To toggle commenting on the current line or selected lines.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Replace</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + H</p>
-                    <p>Cmd + Option + F</p>
+                    {os ? (<p>Ctrl + H</p>)
+                      : (<p>Cmd + Option + F</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To find and replace a string within the current file.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Go to Line</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + G</p>
-                    <p>Cmd + G</p>
+                    {os ? (<p>Ctrl + G</p>) :
+                      (<p>Cmd + G</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To navigate to a specific line number in the file.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Duplicate Line</td>
                   <td className="border px-4 py-2">
-                    <p>Shift + Alt + Down</p>
-                    <p>Shift + Option + Down</p>
+                    {os ? (<p>Shift + Alt + Down</p>)
+                      : (<p>Shift + Option + Down</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To duplicate the current line or selection below.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Move Line Up/Down</td>
                   <td className="border px-4 py-2">
-                    <p>Alt + Up/Down</p>
-                    <p>Option + Up/Down</p>
+                    {os ? (<p>Alt + Up/Down</p>):
+                    (<p>Option + Up/Down</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To move the current line or selection up or down.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">Split Editor</td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + \</p>
-                    <p>Cmd + \</p>
+                    {os ? (<p>Ctrl + \</p>) :
+                      (<p>Cmd + \</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To split the current editor into two.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">
                     Toggle Sidebar Visibility
                   </td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + B</p>
-                    <p>Cmd + B</p>
+                    {os ? (<p>Ctrl + B</p>) :
+                      (<p>Cmd + B</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To show or hide the sidebar.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
                 <tr>
                   <td className="border px-4 py-2">
                     Multi-cursor (add cursor above/below)
                   </td>
                   <td className="border px-4 py-2">
-                    <p>Ctrl + Alt + Up/Down</p>
-                    <p>Option + Cmd + Up/Down</p>
+                    {os ? (<p>Ctrl + Alt + Up/Down</p>)
+                      : (<p>Option + Cmd + Up/Down</p>)}
                   </td>
                   <td className="border px-4 py-2">
                     To add multiple cursors for simultaneous editing.
                   </td>
-                  <td className="border px-4 py-2">General Windows/Mac</td>
+                  <td className="border px-4 py-2">{os}</td>
                 </tr>
               </tbody>
             </table>
